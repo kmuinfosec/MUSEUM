69bc398a7c1a5c8961ad1498d16b29ec09b08fce
Junnyung-Heo <hurjn96@gmail.com>
2020-09-28 14:23:37 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setup_requires = [
     ]
 
 install_requires = [
-    'cycler == 0.10.0',
-    'elasticsearch == 7.9.1',
-    'kiwisolver == 1.2.0',
-    'numpy == 1.19.2',
-    'pyparsing == 2.4.7',
-    'python - dateutil == 2.8.1',
-    'six == 1.15.0',
-    'tqdm == 4.50.0',
-    'urllib3 == 1.25.10'
+    'cycler==0.10.0',
+    'elasticsearch==7.9.1',
+    'kiwisolver==1.2.0',
+    'numpy==1.19.2',
+    'pyparsing==2.4.7',
+    'python-dateutil==2.8.1',
+    'six==1.15.0',
+    'tqdm==4.50.0',
+    'urllib3==1.25.10'
 ]
 
 dependency_links = [
