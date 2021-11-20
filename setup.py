d4c914d9673f7dc30462b9536e17f03e8004ac7f
JunNyung-Hur <hurjn96@gmail.com>
2021-11-20 22:23:20 +0900

    update setpy.py

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@ from setuptools import setup, find_packages
 setup_requires = [
     ]
 
-install_requires = [
-    'cycler==0.11.0',
-    'elasticsearch==7.15.2',
-    'kiwisolver==1.3.2',
-    'numpy==1.21.4',
-    'pyparsing==3.0.6',
-    'python-dateutil==2.8.2',
-    'six==1.16.0',
-    'tqdm==4.62.3',
-    'urllib3==1.26.7'
-]
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 dependency_links = [
     ]
@@ -25,7 +16,7 @@ setup(
     author='KMU Infosec',
     author_email='hurjn96@kookmin.ac.kr',
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=required,
     setup_requires=setup_requires,
     dependency_links=dependency_links,
     data_files=[('museum/module', [
